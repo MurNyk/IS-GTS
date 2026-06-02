caa56b14900fede16c89b654c78de4e883321251
MurNyk <160214917+MurNyk@users.noreply.github.com>
2026-06-02 11:41:49 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IS_GTS
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int CurrentUserID { get; set; } = 0;
+        public static string CurrentUserName { get; set; } = "Гость";
+        public static string CurrentUserLogin { get; set; } = "";
+        public static int CurrentUserRole { get; set; } = 0;
+    }
+}
